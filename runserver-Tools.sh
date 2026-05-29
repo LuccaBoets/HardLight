102ed07c4716d3547f020cf7564474bfcff47ba3
fenndragon <fenndragon@gmail.com>
2026-05-29 12:51:06 -0600

    fix

--- runserver-Tools.sh
+++ runserver-Tools.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-dotnet run --project Content.Server --configuration Tools
+
+CONFIG_FILE="$(pwd)/server_config.local.toml"
+
+if [ -f "$CONFIG_FILE" ]; then
+	dotnet run --project Content.Server --configuration Tools -- --config-file "$CONFIG_FILE"
+else
+	dotnet run --project Content.Server --configuration Tools
+fi
+
 read -p "Press enter to continue"
--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-dotnet run --project Content.Server
+
+CONFIG_FILE="$(pwd)/server_config.local.toml"
+
+if [ -f "$CONFIG_FILE" ]; then
+	dotnet run --project Content.Server -- --config-file "$CONFIG_FILE"
+else
+	dotnet run --project Content.Server
+fi
+
 read -p "Press enter to continue"
