175455e7d86c194d583b7cbff9e28e54799628e4
fenndragon <fenndragon@gmail.com>
2026-05-29 13:07:45 -0600

    Revert "fix"
    This reverts commit 102ed07c4716d3547f020cf7564474bfcff47ba3.

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-CONFIG_FILE="$(pwd)/server_config.local.toml"
-
-if [ -f "$CONFIG_FILE" ]; then
-	dotnet run --project Content.Server -- --config-file "$CONFIG_FILE"
-else
-	dotnet run --project Content.Server
-fi
-
+dotnet run --project Content.Server
 read -p "Press enter to continue"
